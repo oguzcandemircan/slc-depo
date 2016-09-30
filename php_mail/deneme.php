cf6901a978458ccfdfc85a004f4959d5bcb50d29
oguzcandemircan <oguzcandemircann@gmail.com>
2016-09-30 18:03:40 +0300

    Add files via upload

---
+++ php_mail/deneme.php
@@ -0,0 +1,21 @@
+<?php
+$url = "http://www.howtogeek.com";
+
+$str = file_get_contents($url);
+
+
+
+function get_url_contents($url){
+        $crl = curl_init();
+        $timeout = 5;
+        curl_setopt ($crl, CURLOPT_URL,$url);
+        curl_setopt ($crl, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt ($crl, CURLOPT_CONNECTTIMEOUT, $timeout);
+        $ret = curl_exec($crl);
+        curl_close($crl);
+        return $ret;
+}
+
+$veri=get_url_contents($url);
+echo $veri;
+?>
---
+++ php_mail/index.php
@@ -0,0 +1,28 @@
+﻿<?php
+
+		
+	
+		include 'class.phpmailer.php';
+		$mail = new PHPMailer();
+		$mail->IsSMTP();
+		$mail->SMTPAuth = true;
+		$mail->Host = 'host';
+		$mail->Port = 587;
+		//$mail->SMTPSecure = 'ssl';
+		$mail->Username = 'username';
+		$mail->Password = 'password';
+		$mail->SetFrom($mail->Username, 'test_user');
+		$mail->AddAddress("mail","test_");
+		$mail->CharSet = 'UTF-8';
+		$mail->Subject = "title";
+		$content ="content";
+		$mail->MsgHTML($content);
+		if($mail->Send()) {
+			
+			echo '<div class="alert alert-success">E-posta başarıyla gönderildi</div>';
+		} else {
+			
+			echo '<div class="alert alert-success">'.$mail->ErrorInfo.'</div>';
+		}
+		
+?>

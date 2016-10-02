fd0b3d1419c93553cb6582fcbfb7e39dce073344
oguzcan <demircanoguzcan@gmail.com>
2016-10-03 00:10:02 +0300

    test

---
+++ google/test.php
@@ -0,0 +1,9 @@
+<!--
+//print_r(getimagesize("https://img-s1.onedio.com/id-57ee3c7e596dff604e4b1694/rev-0/w-460/h-290/f-jpg/s-2c4e0d1ecf3a140cf40c4db624e0db59e8562a4e.jpg"));
+/*
+$thefile = filesize('index.php'); 
+
+$dosya=file_get_contents("https://img-s1.onedio.com/id-575d0b418d8892fb48cd5c10/rev-0/w-190/h-110/f-jpg/s-a44968c4ea04c7277a1f448ba4da75929bf364e9.jpg");
+echo $dosya;
+*/
+-->
